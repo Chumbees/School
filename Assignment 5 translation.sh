42768b68d05cd25440a192d253028345c9292178
Chumbees <nickchambers11297@gmail.com>
2018-11-01 15:42:14 -0400

    split files

--- Assignment 5 translation.sh
+++ Assignment 5 translation.sh
@@ -3,12 +3,12 @@ MODEL_DIR=models
 rm -Rf $MODEL_DIR
 mkdir -p $MODEL_DIR
 python3 -m sockeye.train\
-                       --source $DATA_DIR/hamlet_modern.snt.aligned \
-                       --target $DATA_DIR/hamlet_original.snt.aligned \
+                       --source $DATA_DIR/modernCombined.snt.aligned \
+                       --target $DATA_DIR/originalCombined.snt.aligned \
                        --encoder cnn \
                        --decoder cnn \
                        --rnn-num-hidden 2 \
-                       --validation-source $DATA_DIR/hamlet_modern.snt.aligned \
-                       --validation-target $DATA_DIR/hamlet_original.snt.aligned \
+                       --validation-source $DATA_DIR/modernCombined.snt.aligned \
+                       --validation-target $DATA_DIR/originalCombined.snt.aligned \
                        --output $MODEL_DIR \
                        --decode-and-evaluate 500

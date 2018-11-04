2b4ee11df006696f994fc64bb49c16ad7c7ca2b8
Chumbees <nickchambers11297@gmail.com>
2018-11-04 13:06:59 -0500

    restructure again, decrease validation to 1000 lines

--- Assignment 5 translation.sh
+++ Assignment 5 translation.sh
@@ -3,15 +3,15 @@ MODEL_DIR=models
 rm -Rf $MODEL_DIR
 mkdir -p $MODEL_DIR
 python3 -m sockeye.train\
-                       --source $DATA_DIR/Train_Modern/Modern_train_shuffled.snt.aligned \
-                       --target $DATA_DIR/Train_Original/Original_train_shuffled.snt.aligned \
+                       --source $DATA_DIR/modern_train.snt.aligned \
+                       --target $DATA_DIR/original_train.snt.aligned \
                        --encoder cnn \
                        --decoder cnn \
                        --rnn-num-hidden 2 \
-                       --validation-source $DATA_DIR/Test_Modern/Modern_test_shuffled.snt.aligned \
-                       --validation-target $DATA_DIR/Test_Original/Original_test_shuffled.snt.aligned \
+                       --validation-source $DATA_DIR/modern_validation.snt.aligned \
+                       --validation-target $DATA_DIR/original_validation.snt.aligned \
                        --output $MODEL_DIR \
                        --decode-and-evaluate 500 \
-                       --batch-size 100 \
+                       --batch-size 200 \
                        --batch-type word \
                        --initial-learning-rate .0003

6095564b5c4dedd8147be3ef51be25d78dbfb182
Chumbees <nickchambers11297@gmail.com>
2018-11-07 12:32:15 -0500

    code again

--- Assignment 5 translation.sh
+++ Assignment 5 translation.sh
@@ -9,10 +9,10 @@ python3 -m sockeye.train\
                        --decoder cnn \
                        --cnn-activation-type relu \
                        --cnn-hidden-dropout .05 \
-                       --rnn-num-hidden 512 \
+                       --rnn-num-hidden 256 \
                        --rnn-attention-type bilinear \
                        --optimizer adam \
-                       --max-num-epochs 400 \
+                       --max-num-epochs 300 \
                        --validation-source $DATA_DIR/modern_validation.snt.aligned \
                        --validation-target $DATA_DIR/original_validation.snt.aligned \
                        --output $MODEL_DIR \
